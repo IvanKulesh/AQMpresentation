4920a2e153eca1318d46e5e69ccfa107cbc2f8c5
MPapendrecht <M.N.G.Papendrecht@student.tudelft.nl>
2018-03-04 01:05:09 +0100

    Updated to lambdaF units (now 1 lambdaF unit wide fermi hole :))

--- images/Density.m
+++ images/Density.m
@@ -1,9 +1,10 @@
 close all
-x = linspace(-10,10,1000);
-y = 1-9.*((sin(x)-x.*cos(x))./x.^3).^2;
-plot(x,y,'LineWidth',2)
+r = linspace(-2,2,1000);
+X = r.*(2*pi);
+y = 1-9.*((sin(X)-X.*cos(X))./X.^3).^2;
+plot(r,y,'LineWidth',2)
 hold on
-w = 3;
+w = 0.5;
 plot([-w,-w],[0,1],'--r',[w,w],[0,1],'--r')
 set(gca,'FontSize',30)
 xlabel("$r [\lambda_F]$" ,'Interpreter','latex')
